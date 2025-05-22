115a7b3a8da2ae491597729773565e245c9607c2
hhyunn <cha9164@naver.com>
2025-05-22 19:58:53 +0900

    feat: Skeleton UI 적용

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -27,9 +27,23 @@ export default function CurrentWeather() {
     <div className="pt-12 pb-2 px-2">
       <h2 className="sr-only">현재 날씨</h2>
       <div className="text-center">
-        <h1 className="text-2xl font-bold mb-1.5">{currentWeather?.name}</h1>
+        <h1 className="text-2xl font-bold mb-1.5">
+          {currentWeather ? (
+            currentWeather.name
+          ) : (
+            <span className="inline-block w-24 h-6 bg-white/40 animate-pulse rounded-lg" />
+          )}
+        </h1>
         <h2>
-          {showClothing ? recommendClothes(Math.round(currentWeather?.main.temp)) : currentWeather?.weather[0].main}
+          {currentWeather ? (
+            showClothing ? (
+              recommendClothes(Math.round(currentWeather?.main.temp))
+            ) : (
+              currentWeather.weather[0].main
+            )
+          ) : (
+            <span className="inline-block w-14 h-5 bg-white/40 animate-pulse rounded-lg" />
+          )}
         </h2>
         <p className="my-4">
           {currentWeather ? (
@@ -37,7 +51,7 @@ export default function CurrentWeather() {
               src={
                 showClothing
                   ? `/images/${clothImage(Math.round(currentWeather?.main.temp))}`
-                  : `https://openweathermap.org/img/wn/${currentWeather?.weather[0].icon}@2x.png`
+                  : `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`
               }
               alt="날씨 이미지"
               width={80}
@@ -46,21 +60,33 @@ export default function CurrentWeather() {
               priority
             />
           ) : (
-            <span>날씨 이미지</span>
+            <span className="inline-block w-20 h-20 bg-white/40 animate-pulse rounded-full" />
           )}
         </p>
-        <strong className="block text-5xl mb-1">{`${Math.round(currentWeather?.main.temp)}º`}</strong>
-        {<span>체감 {Math.round(currentWeather?.main.feels_like)}º</span>}
+        <strong className="block text-5xl mb-1">
+          {currentWeather ? (
+            `${Math.round(currentWeather.main.temp)}º`
+          ) : (
+            <span className="inline-block w-16 h-10 bg-white/40 animate-pulse rounded-lg" />
+          )}
+        </strong>
+        {currentWeather ? (
+          <span>체감 {Math.round(currentWeather.main.feels_like)}º</span>
+        ) : (
+          <span className="inline-block w-16 h-5 bg-white/40 animate-pulse rounded-lg" />
+        )}
       </div>
-      {
+      {currentWeather ? (
         <ToggleButton
-          temp={currentWeather?.main.temp}
-          locationName={currentWeather?.name}
-          icon={currentWeather?.weather[0].icon}
+          temp={currentWeather.main.temp}
+          locationName={currentWeather.name}
+          icon={currentWeather.weather[0].icon}
           showClothing={showClothing}
           toggleView={toggleView}
         />
-      }
+      ) : (
+        <span className="inline-block w-28 h-12 bg-white/40 animate-pulse rounded-lg" />
+      )}
     </div>
   );
 }
--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -28,10 +28,9 @@ export default function Forecast() {
 
   return (
     <div className="px-2 py-2">
-      {
+      {forecast ? (
         <div className="p-3 rounded-lg bg-[rgba(255,255,255,0.3)]">
           <h2 className="mb-3.5">시간별 예보</h2>
-
           <ul className="space-y-2.5">
             {forecast?.list.map((item: ForecastItem, index: number) => {
               return (
@@ -45,7 +44,7 @@ export default function Forecast() {
                   <ul className="flex gap-5 w-[70%]">
                     <li className="flex items-center gap-2 w-1/2">
                       <p className="shrink-0">
-                        {
+                        {forecast ? (
                           <Image
                             src={
                               showClothing
@@ -57,7 +56,9 @@ export default function Forecast() {
                             height={30}
                             className="m-auto"
                           />
-                        }
+                        ) : (
+                          <span className="inline-block w-8 h-8 bg-white/40 animate-pulse rounded-full" />
+                        )}
                       </p>
                       <p className="text-center w-[calc(100%-30px)]">
                         <span className="block text-sm">
@@ -80,7 +81,9 @@ export default function Forecast() {
             })}
           </ul>
         </div>
-      }
+      ) : (
+        <span className="inline-block w-full h-screen bg-white/40 animate-pulse rounded-lg" />
+      )}
     </div>
   );
 }
