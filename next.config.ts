3fc63834113c02f8ca02c4d49291a43233d7fd6d
hhyunn <cha9164@naver.com>
2025-05-23 15:08:05 +0900

    chore: images.domains를 images.remotePatterns로 변경

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["openweathermap.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      },
+    ],
   },
 };
 
